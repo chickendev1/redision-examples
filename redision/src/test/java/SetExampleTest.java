9ae97382ab45a4084b1e21e302fc4617b2b7fe87
chicken-dev <chickendeveloper1@gmail.com>
2018-04-05 17:26:16 +0700

    SetTest Examples (cont.)

--- redision/src/test/java/SetExampleTest.java
+++ redision/src/test/java/SetExampleTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.Arrays;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,32 +32,34 @@ public class SetExampleTest {
         originalSet1 = redissionClient.getSet("mySet1");
         originalSet1.add("1");
         originalSet1.add("2");
+        originalSet1.add("3");
         
         originalSet2 = redissionClient.getSet("mySet2");
+        originalSet2.add("2");
         originalSet2.add("3");
-        originalSet2.add("4");
     }
 
     @Test
     public void setTest1() {
         RSet<String> set = redissionClient.getSet("mySet1");
-        Assert.assertEquals(2, set.size());
+        Assert.assertEquals(3, set.size());
         Assert.assertTrue(set.contains("1"));
         Assert.assertTrue(set.containsAll(originalSet1));
+        Assert.assertTrue(set.containsAll(originalSet2)); // set1 and set2 are different in size but all set2's elements are inside set1
     }
     
     @Test
     public void setTest2() {
         RSet<String> set1 = redissionClient.getSet("mySet1");
-        Assert.assertEquals(2, set1.size());
+        Assert.assertEquals(3, set1.size());
         RSet<String> set2 = redissionClient.getSet("mySet2");
         Assert.assertEquals(2, set2.size());
         
-        set1.union(set2.getName()); // replace all elements in set1 by set2's elements
+        set1.remove("3");
         Assert.assertEquals(2, set1.size());
-        Assert.assertTrue(set1.containsAll(Arrays.asList("3", "4")));
+        Assert.assertFalse(set1.containsAll(set2));
     }
-
+    
     @After
     public void tearDown() {
         RSet<String> set1 = redissionClient.getSet("mySet1");
