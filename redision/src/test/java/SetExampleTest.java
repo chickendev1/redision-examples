e9203c14fe2bc1b8aca51cb7bfcc4c35851afd0a
chicken-dev <chickendeveloper1@gmail.com>
2018-04-05 16:56:25 +0700

    SetTest Examples
    More test for RSet

--- redision/src/test/java/SetExampleTest.java
+++ redision/src/test/java/SetExampleTest.java
@@ -3,53 +3,70 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package collectionExamples;
-
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.Assert;
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.redisson.api.RSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.Redisson;
 
 /**
  *
  * @author thanh.mai
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SetExampleTest {
-
+    
+    private RSet<String> originalSet1;
+    private RSet<String> originalSet2;
+    private RedissonClient redissionClient;
+    
     @Before
     public void setUp() {
         // connects to 127.0.0.1:6379 by default
-        RedissonClient redisson = Redisson.create();
 
-        RSet<String> set = redisson.getSet("mySet1");
-        set.add("1");
-        set.add("2");
-
-        redisson.shutdown();
+        redissionClient = Redisson.create();
+        originalSet1 = redissionClient.getSet("mySet1");
+        originalSet1.add("1");
+        originalSet1.add("2");
+        
+        originalSet2 = redissionClient.getSet("mySet2");
+        originalSet2.add("3");
+        originalSet2.add("4");
     }
 
     @Test
-    public void createSetTest() {
-        RedissonClient redisson = Redisson.create();
-        
-        RSet<String> set = redisson.getSet("mySet1");
+    public void setTest1() {
+        RSet<String> set = redissionClient.getSet("mySet1");
         Assert.assertEquals(2, set.size());
+        Assert.assertTrue(set.contains("1"));
+        Assert.assertTrue(set.containsAll(originalSet1));
+    }
+    
+    @Test
+    public void setTest2() {
+        RSet<String> set1 = redissionClient.getSet("mySet1");
+        Assert.assertEquals(2, set1.size());
+        RSet<String> set2 = redissionClient.getSet("mySet2");
+        Assert.assertEquals(2, set2.size());
         
-        redisson.shutdown();
-
+        set1.union(set2.getName()); // replace all elements in set1 by set2's elements
+        Assert.assertEquals(2, set1.size());
+        Assert.assertTrue(set1.containsAll(Arrays.asList("3", "4")));
     }
 
     @After
     public void tearDown() {
-        RedissonClient redisson = Redisson.create();
+        RSet<String> set1 = redissionClient.getSet("mySet1");
+        set1.delete();
         
-        RSet<String> set = redisson.getSet("mySet1");
-        set.delete();
+        RSet<String> set2 = redissionClient.getSet("mySet2");
+        set2.delete();
         
-        redisson.shutdown();
-
+        redissionClient.shutdown();
     }
 }
